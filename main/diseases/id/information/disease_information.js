5ea847e4d5914863c62ad9bb650fa4fc26f33d13
Ryan Bradford <ryanbradford333@gmail.com>
2018-07-24 12:54:32 -0400

    Wrote disease info view on iOS
    Also added the actual person image, cleaned up some text, and made things look better

--- main/diseases/id/information/disease_information.js
+++ main/diseases/id/information/disease_information.js
@@ -1,12 +1,14 @@
 function get_disease_information(req, res) {
     let disease_name = req.params.disease_name;
     var to_send;
-    if(disease_name === "Flu") {
+    ///if(disease_name === "Influenza (Flu)") {
         to_send = {
-            medicine: ["motrin"],
-            doctor: true
+            disease_name: "Influenza (Flu)",
+            medicines: ["motrin"],
+            doctor: 0, //0 = no, 1 = potential, 2 = definitely
+            description: "This is a disease that has a lot of words."
         }
-    }
+    //}
     req.http_responses.report_success_with_info(req, res, to_send);
 }
 
